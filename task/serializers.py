d703460e3d995608359d45b27750d5df6a22276e
lekan2 <hertheywerelay@gmail.com.com>
2023-09-15 06:01:17 -0700

    first commit

--- task/serializers.py
+++ task/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from djongo.core.exceptions import ValidationError
+#from django.core.exceptions import ValidationError
 from .models import Person
 
 class PersonSerializer(serializers.ModelSerializer):
@@ -7,7 +7,8 @@ class PersonSerializer(serializers.ModelSerializer):
         model = Person
         fields = "__all__"
 
-        def validate_name(self, value):
-            if any(char.isdigit() for char in value):
-                raise serializers.ValidationError("Name can't be numbers.")
-                return value
+
+    def validate_name(self, value):
+        if any(char.isdigit() for char in value):
+            raise serializers.ValidationError("Name cannot contain numbers.")
+            return value
